8ff91fabdc79c2eb07319d5df5d16a32b022d070
Leonardo Godde <leogodde@webgoal.com.br>
2025-03-26 16:00:28 -0300

    refatora home page e containers de personagens

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../datasources/remote_datasource.dart';
 import '../model/character_model.dart';
@@ -22,114 +23,86 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-  final List<Color> colors = [
-    Color(0xFF1ABC9C),
-    Color(0xFF2ECC71),
-    Color(0xFF3498DB),
-    Color(0xFF9B59B6),
-    Color(0xFFF1C40F),
-    Color(0xFFE67E22),
-    Color(0xFFE74C3C),
-    Color(0xFF34495E),
-    Color(0xFF16A085),
-    Color(0xFF27AE60),
-    Color(0xFF2980B9),
-    Color(0xFF8E44AD),
-    Color(0xFFF39C12),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Dragon Ball"),
+      backgroundColor: const Color(0xFFE98E03),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(124),
+        child: Container(
+          padding: const EdgeInsets.only(left: 24),
+          width: double.infinity,
+          height: 124,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Image.asset('assets/img/db_logo.png'),
+              const SizedBox(width: 8),
+              Text(
+                'Wiki',
+                style: GoogleFonts.knewave(
+                  textStyle: const TextStyle(color: Colors.black, fontSize: 32),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: FutureBuilder(
         future: characters,
         builder: (context, snapshot) {
           final List<Character> data = snapshot.data?.characters ?? [];
 
-          return Visibility(
-            visible: data.isNotEmpty,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 180,
-                  width: double.infinity,
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    separatorBuilder: (context, index) => const SizedBox(width: 10),
-                    itemBuilder: (context, index) {
-                      final color = colors[index % colors.length];
-
-                      return Stack(
-                        children: [
-                          Container(
-                            width: 110,
-                            height: 145,
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            decoration: BoxDecoration(
-                              color: color,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: RotatedBox(
-                              quarterTurns: 3,
-                              child: Text(
-                                data[index].name,
-                                textDirection: TextDirection.rtl,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+          return SizedBox(
+            height: 400,
+            width: double.infinity,
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(width: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              itemBuilder: (context, index) {
+                return Stack(
+                  children: [
+                    Container(
+                      width: 225,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFFCA07),
+                        borderRadius: BorderRadius.circular(36),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 32),
+                          child: RotatedBox(
+                            quarterTurns: -1,
+                            child: Text(
+                              data[index].name.toUpperCase(),
+                              style: GoogleFonts.knewave(
+                                textStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 24,
+                                  // fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
                           ),
-                          Positioned(
-                            top: 40,
-                            left: 10,
-                            child: Image.network(
-                              data[index].image,
-                              width: 140,
-                              height: 140,
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                    itemCount: 10,
-                    scrollDirection: Axis.horizontal,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Expanded(
-                  child: ListView.separated(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                data[index].name,
-                                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                              Text("Ki: ${data[index].ki}"),
-                              Text("Raça: ${data[index].race}"),
-                            ],
-                          ),
                         ),
-                      );
-                    },
-                    itemCount: data.length,
-                    separatorBuilder: (context, index) => const SizedBox(height: 10),
-                  ),
-                ),
-              ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.network(
+                        data[index].image,
+                        height: 385,
+                        width: 225,
+                      ),
+                    ),
+                  ],
+                );
+              },
+              itemCount: data.length,
+              scrollDirection: Axis.horizontal,
             ),
           );
         },
