5dc6a9e0774350dd734e42b2ad4d699bdb16e744
Ademir <ademir@webgoal.com.br>
2025-03-26 14:21:29 -0300

    Implements cards [WIP]

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -24,6 +24,22 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  final List<Color> colors = [
+    Color(0xFF1ABC9C),
+    Color(0xFF2ECC71),
+    Color(0xFF3498DB),
+    Color(0xFF9B59B6),
+    Color(0xFFF1C40F),
+    Color(0xFFE67E22),
+    Color(0xFFE74C3C),
+    Color(0xFF34495E),
+    Color(0xFF16A085),
+    Color(0xFF27AE60),
+    Color(0xFF2980B9),
+    Color(0xFF8E44AD),
+    Color(0xFFF39C12),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,16 +52,46 @@ class _HomePageState extends State<HomePage> {
         builder: (context, snapshot) {
           final List<Character> data = snapshot.data?.characters ?? [];
 
-          return ListView.builder(
-            itemBuilder: (context, index) => SizedBox(
-              height: 40,
-              width: 40,
-              child: Image.network(
-                data[index].image,
-              ),
+          return Container(
+            height: 200,
+            color: Colors.red,
+            width: double.infinity,
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(width: 10),
+              itemBuilder: (context, index) {
+                final color = colors[index % colors.length];
+
+                return Stack(
+                  children: [
+                    Container(
+                      width: 110,
+                      height: 145,
+                      decoration: BoxDecoration(
+                        color: color,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(data[index].name,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                    Positioned(
+                      top: 40,
+                      left: 10,
+                      child: Image.network(
+                        data[index].image,
+                        width: 140,
+                        height: 140,
+                      ),
+                    ),
+                  ],
+                );
+              },
+              itemCount: data.length,
+              scrollDirection: Axis.horizontal,
             ),
-            itemCount: data.length,
-            scrollDirection: Axis.horizontal,
           );
         },
       ),
